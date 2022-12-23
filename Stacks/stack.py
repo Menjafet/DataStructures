b734af3405cda55b5f749de583e3d6285666b3c1
menjafet <119637452+menjafet@users.noreply.github.com>
2022-12-22 23:53:03 -0600

    First commit

---
+++ Stacks/stack.py
@@ -0,0 +1,14 @@
+
+
+class MyClass:
+
+    count: int
+    head: str
+
+    def __init__(self):
+       pass
+    
+
+    def efe(self):
+        print("pp")
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import Stacks.stack
+
+class main():
+
+  def __init__(self, dt):
+    self.dt= dt
+    match dt:
+        case 0:
+            Stacks.stack.MyClass().efe()
+
+  
+
+# 0=stack
+staquita= main(0)
